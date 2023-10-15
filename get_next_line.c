d1bbe5edc16381dd4230e85995d1ef18dc0dbc2c
yliu <yliu@XLIJFELKNIX.local>
2023-10-15 10:37:34 +0900

    fix read mem leak by adding null char at last str

--- get_next_line.c
+++ get_next_line.c
@@ -31,7 +31,9 @@ static char	*get_whole_str_from_read(int fd, char *whole_str)
 		bytes_read = read(fd, buf, BUFFER_SIZE);
 		if (bytes_read == READ_ERROR | bytes_read == READ_END)
 			break;
-		printf("buf: %s\n", buf);
+		buf[bytes_read] = '\0';
+		printf("\nbuf: %s\n", buf);
+		printf("line_size: %zu\n", line_size);
 		if (buf == NULL)
 			break ;
 		whole_str = ft_strjoin(whole_str, buf);
@@ -48,36 +50,36 @@ static char	*get_whole_str_from_read(int fd, char *whole_str)
 	return (whole_str);
 }
 
-//static char	*get_one_line(char *whole_str)
-//{
-//	char	*line;
-//	size_t	line_size;
-//
-//	line_size = ft_strchr(whole_str, '\n') - whole_str;
-//	line = malloc(sizeof(char) * line_size + 1);
-//	if (line == NULL)
-//		return (NULL);
-//	ft_strlcpy(line, whole_str, line_size + 1);
-//	return (line);
-//}
+static char	*get_one_line(char *whole_str)
+{
+	char	*line;
+	size_t	line_size;
 
-//static char	*get_next_whole_str(char *whole_str)
-//{
-//	char	*dst;
-//	size_t	line_size;
-//
-//	line_size = ft_strchr(whole_str, '\n') - whole_str;
-//	dst = malloc(sizeof(char) * line_size + 1);
-//	if (dst == NULL)
-//		return (NULL);
-//	ft_strlcpy(dst, whole_str + line_size , BUFFER_SIZE);
-//	return (dst);
-//}
+	line_size = ft_strchr(whole_str, '\n') - whole_str;
+	line = malloc(sizeof(char) * line_size + 1);
+	if (line == NULL)
+		return (NULL);
+	ft_strlcpy(line, whole_str, line_size + 1);
+	return (line);
+}
+
+static char	*get_next_whole_str(char *whole_str)
+{
+	char	*dst;
+	size_t	line_size;
+
+	line_size = ft_strchr(whole_str, '\n') - whole_str;
+	dst = malloc(sizeof(char) * line_size + 1);
+	if (dst == NULL)
+		return (NULL);
+	ft_strlcpy(dst, whole_str + line_size , BUFFER_SIZE);
+	return (dst);
+}
 
 char	*get_next_line(int fd)
 {
 	static char	*whole_str = "";
-	//char		*line;
+	char		*line;
 
 	// 1.
 	whole_str = get_whole_str_from_read(fd, whole_str);
@@ -85,20 +87,19 @@ char	*get_next_line(int fd)
 		return (NULL);
 	printf("whole: %s\n", whole_str);
 
-//	// 2.
-//	line = get_one_line(whole_str);
-//	if (line == NULL)
-//		return (NULL);
-//	printf("line: %s\n", line);
+	// 2.
+	line = get_one_line(whole_str);
+	if (line == NULL)
+		return (NULL);
+	printf("line: %s\n", line);
 
-//	// 3.
-//	whole_str = get_next_whole_str(whole_str);
-//	if (whole_str == NULL)
-//		return (NULL);
-//	printf("whole2: %s\n", whole_str);
+	// 3.
+	whole_str = get_next_whole_str(whole_str);
+	if (whole_str == NULL)
+		return (NULL);
+	printf("whole2: %s\n", whole_str);
 
-	return (NULL);
-//	return (line);
+	return (line);
 }
 
 ////////////////////////////////////////// test function
@@ -118,7 +119,7 @@ int	main(void)
 	printf("%s\n", result);
 	puts("#### first line ####\n");
 
-//	result = get_next_line(fd1);
+	//result = get_next_line(fd1);
 //	puts("\n#### second line ####");
 //	printf("%s\n", result);
 //	puts("#### second line ####\n");
